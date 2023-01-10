eea083c5fd5e5bdc8590eae99b6364c31fbc9cdf
christianangelov <31957091+christianangelov@users.noreply.github.com>
2023-01-10 01:12:30 +0100

    Add files via upload

---
+++ DrugExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonBase
+{
+    public static class DrugExtension
+    {
+      
+
+        public static bool CheckDrugNumber(string number)
+        {
+            var result = number.Length == 10;
+            int res = 0;
+
+            for (int i = 0; i < number.Length - 1; i++)
+            {
+                res += (number[i] - '0') * (i + 1);
+            }
+            res = res % 11;
+
+            if (res == 10)
+            {
+                if (number[^1] == 'X')
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+            else
+            {
+                if (res == number[9] - '0')
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+        public static bool CheckDesignation(string designation)
+        {
+            if (designation.Length < 10)
+            {
+                return false;
+            }
+            return true;
+
+        }
+    }
+}
---
+++ PatientExtension.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonBase
+{
+    public static class PatientExtension
+    {
+        public static bool CheckFirstname(string firstName)
+        {
+            if (firstName.Length < 3)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool CheckLastName(string lastName)
+        {
+            if (lastName.Length < 3)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool CheckSSN(string ssn)
+        {
+            var result = ssn.Length == 10;
+            int[] weight = new int[] { 3, 7, 9, 5, 8, 4,2,1,6 };
+            int sum = 0;
+            for (int i = 0; i < ssn.Length -1; i++)
+            {
+                if (Char.IsLetter(ssn[i])) return false;
+                if (i < 3)
+                {
+                    sum += ((ssn[i] - '0') * weight[i]);
+                }
+                else
+                {
+                    sum += ((ssn[i + 1] - '0') * weight[i]);
+                }
+                
+                
+            }
+            int checksum = sum % 11;
+            if((ssn[3] - '0') == checksum)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
